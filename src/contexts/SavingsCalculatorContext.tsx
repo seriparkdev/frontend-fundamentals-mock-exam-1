3f0e925c757ad4c3f83618cb3f1603136ff49207
seriparkdev <parkseridev@gmail.com>
2025-11-23 21:26:58 +0900

    feat: 적금 계산기 관련 컴포넌트 간 상태 공유를 위한 Context 생성

---
+++ src/contexts/SavingsCalculatorContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode, useContext } from 'react';
+import { SavingsProduct } from 'types/savingsProduct';
+
+interface ContextValue {
+  targetAmount: string;
+  setTargetAmount: (amount: string) => void;
+
+  monthlyAmount: string;
+  setMonthlyAmount: (amount: string) => void;
+
+  savingsPeriod: number;
+  setSavingsPeriod: (terms: number) => void;
+
+  filteredProducts: SavingsProduct[];
+}
+
+const SavingsCalculatorContext = createContext<ContextValue | undefined>(undefined);
+
+export function SavingsCalculatorProvider({ value, children }: { value: ContextValue; children: ReactNode }) {
+  return <SavingsCalculatorContext.Provider value={value}>{children}</SavingsCalculatorContext.Provider>;
+}
+
+export function useSavingsCalculatorContext() {
+  const context = useContext(SavingsCalculatorContext);
+
+  if (context === undefined) {
+    throw new Error('useSavingsCalculatorContext은 SavingsCalculatorProvider 내에서 사용되어야 합니다.');
+  }
+
+  return context;
+}
