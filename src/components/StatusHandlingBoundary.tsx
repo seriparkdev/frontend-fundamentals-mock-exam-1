d22e898edb8f3fe1c2cd4607077e4da68eb10db0
seriparkdev <parkseridev@gmail.com>
2025-11-24 02:41:39 +0900

    refactor: 에러 바운더리 컴포넌트 수정(suspense 표출 시간 조정 및 에러 문구)

--- src/components/StatusHandlingBoundary.tsx
+++ src/components/StatusHandlingBoundary.tsx
@@ -1,29 +1,31 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import { colors, ListRow } from 'tosslib';
-import { parseError } from 'utils/errorParser';
+import { ListRow } from 'tosslib';
 
-const ErrorFallback = ({ error }: { error: Error }) => {
-  const errorData = parseError(error);
-  return (
-    <ListRow
-      contents={
-        <ListRow.Texts
-          type="2RowTypeA"
-          top={`오류 발생 [${errorData.code}]`}
-          topProps={{ fontWeight: 'bold', color: colors.grey900 }}
-          bottom={errorData.message}
-          bottomProps={{ color: colors.grey600 }}
-        />
-      }
-    />
-  );
+const DelayedFallback = () => {
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShown(true);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isShown) {
+    return null;
+  }
+
+  return <ListRow contents={<ListRow.Texts type="1RowTypeA" top="로딩 중" />} />;
 };
 
 export const StatusHandlingBoundary = ({ children }: { children: React.ReactElement }) => {
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Suspense fallback={<ListRow contents={<ListRow.Texts type="1RowTypeA" top="로딩 중" />} />}>{children}</Suspense>
+    <ErrorBoundary
+      fallback={<ListRow contents={<ListRow.Texts type="1RowTypeA" top="네트워크 에러가 발생했습니다." />} />}
+    >
+      <Suspense fallback={<DelayedFallback />}>{children}</Suspense>
     </ErrorBoundary>
   );
 };
